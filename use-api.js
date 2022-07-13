6e5be8603fea002b9aa13075f57ed537f29401e4
Muhammad Shahbaz <38165415+shahbazpro2@users.noreply.github.com>
2022-07-13 10:31:23 +0000

    Add complete response

--- use-api.js
+++ use-api.js
@@ -1,46 +1,51 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react'
 
-const useApi = (apiFun, callback) => {
+const useApi = (apiFun, callback, errCallback) => {
     const [state, setState] = useState({
         loading: false,
         error: null,
         data: null,
+        res: null,
         message: null
     })
 
     useEffect(() => {
         if (apiFun) {
-            processing(apiFun, callback)
+            processing(apiFun, callback, errCallback)
         }
     }, [])
 
-    const executeApi = async (fun, callback) => {
+    const executeApi = async (fun, callback, errCallback) => {
         if (fun) {
-            processing(fun, callback)
+            processing(fun, callback, errCallback)
         }
 
     }
 
-    const processing = async (api, callback) => {
+    const processing = async (api, callback, errCallback) => {
         setState({ ...state, loading: true })
         let res = null
-        if (api instanceof Function)
-            res = await api()
-        else
-            res = await api
+        if (api instanceof Function) res = await api()
+        else res = await api
+        if (res) {
+            setState({
+                loading: false,
+                error: res.error,
+                message: res.message,
+                data: !res.error ? res.data : null,
+                res: res.res
+            })
 
-        setState({
-            loading: false,
-            error: res.error,
-            message: res.message,
-            data: !res.error ? res.data : null
-        })
-        if (callback)
-            callback(res)
+            if (!res.error) {
+                if (callback) callback(res)
+            }
+            if (res.error) {
+                if (errCallback) errCallback(res)
+            }
+        }
     }
-    const { data, message, error, loading } = state
-    return [executeApi, { data, message, error, loading }]
+    return [executeApi, { ...state }]
 }
 
 export default useApi
